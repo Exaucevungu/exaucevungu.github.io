a550893ca0a2aec1c901fbf7645ad8d762cb5e35
Exaucevungu <exaucevungu5@gmail.com>
2025-09-07 09:30:47 +0100

    Update portefolio.js

--- portefolio.js
+++ portefolio.js
@@ -1,14 +1,14 @@
 
 
 
-const bars = document.getElementById("bars");
+const bars = document.getElementById("bar");
 const menu = document.getElementById("menu");
 const x = document.getElementById("x");
 const header = document.getElementById("header");
 const html = document.getElementById("html");
 var make = window.alert("Welcome to my portfolio!")
 x.style.display = "none";
-bars.style.display= "block";
+bar.style.display= "block";
 html.addEventListener("onload", phrase);
 function phrase(){
 	alert("Welcome to my portfolio!!")
@@ -18,7 +18,7 @@ bars.addEventListener("click", mark);
 function mark(){
 	x.style.display = "block"
 	x.style.float="right";
-	bars.style.display = "none";
+	bar.style.display = "none";
 	header.style.height = "500px";
 	menu.style.display = "flex";
 	menu.style.flexDirection= "column";
@@ -27,12 +27,13 @@ function mark(){
 x.addEventListener("click", markx);
 
 function markx(){
-	bars.style.display = "block";
+	bar.style.display = "block";
 	x.style.display = "none";
 	header.style.height = "100px";
 	menu.style.display = "none";
 	
 }
+
 
 
 
